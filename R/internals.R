760ce57b8953a1e5c670c303d2af7fd69056b429
LimaRAF <raflima@usp.br>
2022-07-07 11:46:57 +0200

    Adding a new internal function (imported from plantR) to remove Latin1 characters

--- R/internals.R
+++ R/internals.R
@@ -38,3 +38,51 @@
    
   return(x)
 }
+#' @title Remove Special Characters
+#'
+#' @description Simple function imported and adapted from package `plantR` to
+#'   replace accents by the same or corresponding letters without accents.
+#'   Currently, the function basically replaces special characters corresponding
+#'   to the Latin-1 Supplement and very few Latin Extended-A.
+#'
+#' @param x a vector with characters to be replaced
+#'
+#' @return the input vector with the special characters replaced
+#'
+#' @keywords internal
+#'
+#' @noRd
+#'
+#' @examples
+#' \dontrun{
+#'   nomes <- c("Thom\u00e9", "Mu\u00f1oz", "\u0153uf")
+#'   nomes
+#'
+#'   .remove_latin(nomes)
+#' }
+#'
+.remove_latin <- function(x) {
+  
+  #Getting the special characters to be replaced
+  unwanted_latin <- names(unwantedLatin)
+  replace_latin <- unwantedLatin
+  
+  #Single-letter replacements
+  replace_latin1 <- replace_latin[nchar(replace_latin) == 1]
+  unwanted_latin1 <- unwanted_latin[nchar(replace_latin) == 1]
+  x <- chartr(
+    paste(unwanted_latin1, collapse = ''),
+    paste(replace_latin1, collapse = ''),
+    x)
+  
+  #Double-letter replacements
+  replace_latin2 <- replace_latin[nchar(replace_latin) == 2]
+  names(replace_latin2) <- unwanted_latin[nchar(replace_latin) == 2]
+  for (i in seq_along(replace_latin2))
+    x <- gsub(names(replace_latin2)[i],
+              replace_latin2[i],
+              x, fixed = TRUE)
+  
+  return(x)
+}
+
